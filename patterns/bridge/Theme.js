fc222cdbf4da3a18926e7b93d064e2abc7a06856
Konrad Zapala <zapala.konrad@gmail.com>
2019-02-27 20:05:12 +0100

    Removed js files (not needed when I have typescript files).

--- patterns/bridge/Theme.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Colour;
-(function (Colour) {
-    Colour[Colour["Red"] = 0] = "Red";
-    Colour[Colour["Blue"] = 1] = "Blue";
-    Colour[Colour["Yellow"] = 2] = "Yellow";
-})(Colour = exports.Colour || (exports.Colour = {}));
-//# sourceMappingURL=Theme.js.map
